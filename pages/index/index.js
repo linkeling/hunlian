55f7561e049b580b0915003c3c24bff96bcd2d54
ynbylkl <1305984923@qq.com>
2020-11-02 21:23:26 +0800

    新建项目

---
+++ pages/index/index.js
@@ -0,0 +1,37 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    tabData: [{
+      telephone:'13987637601',
+      name:'第一个',
+      message: 'foo',
+    }, {
+      telephone:'13987637601',
+      name:'第二个',
+      message: 'bar'
+    }, {
+      telephone:'13987637601',
+      name:'随时随地',
+      message: 'bar'
+    }, {
+      telephone:'13987637601',
+      name:'随时随地',
+      message: 'bar'
+    }, {
+      telephone:'13987637601',
+      name:'随时随地',
+      message: 'bar'
+    }, {
+      telephone:'13987637601',
+      name:'随时随地',
+      message: 'bar'
+    }, {
+      telephone:'13987637601',
+      name:'随时随地',
+      message: 'bar'
+    }]
+  }
+})
---
+++ pages/mine/custom/index.js
@@ -0,0 +1,25 @@
+var app = getApp();
+
+Page({
+  data: {
+     genderList: [{id: 1, name: '男'}, {id: 2, name: '女'}],
+  },
+  bindPickerChange: function (e) {
+   
+  },
+  onLoad: function (options) {
+  },
+  onReady: function () {
+
+  },
+  onShow: function () {
+
+  },
+  onHide: function () {
+    // 页面隐藏
+
+  },
+  onUnload: function () {
+    // 页面关闭
+  }
+})
